8c099ecb79d242de024d6689604a582b80707bea
Lukas Martin Brückner <lukas.brueckner@mailbox.org>
2023-02-24 17:09:32 +0100

    Fixed project dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,18 @@
-import { defineNuxtConfig } from 'nuxt'
-import vuetify from "vite-plugin-vuetify";
+// import { defineNuxtConfig } from 'nuxt/config'
+// import vuetify from "vite-plugin-vuetify";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    build: {
-        transpile: [
-            'vuetify',
-        ],
-    },
     css: [
         '@mdi/font/css/materialdesignicons.min.css',
-        '@/assets/styles/main.scss',
+        'assets/styles/main.scss'
     ],
-    devServerHandlers: [],
-    modules: [
-        '@nuxtjs/google-fonts',
-        '@nuxtjs/tailwindcss',
-        '@pinia/nuxt',
-        'nuxt-icon',
-        async (options, nuxt) => {
-			// @ts-ignore
-			nuxt.hooks.hook("vite:extendConfig", (config) => config.plugins.push(vuetify()));
-		},
-    ],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
-    typescript: {
-        shim: false,
+    build: {
+        transpile: ['vuetify'],
     },
     vite: {
-        ssr: {
-			noExternal: ["vuetify"],
-		},
-		define: {
-			"process.env.DEBUG": false,
-		},
+        define: {
+            'process.env.DEBUG': false,
+        },
     },
 })
